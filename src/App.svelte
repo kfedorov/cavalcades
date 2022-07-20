068a5d379680a4f42b0d7fc03808377cd91c6904
fedorov.konstantin <fedorov.konstantin@gmail.com>
2022-07-20 14:43:27 -0400

    Add tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,9 @@
 <main>
+  <div class="p-4 bg-black text-white">Tailwind works</div>
 </main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import sveltePreprocess from 'svelte-preprocess'
+import tailwind from "tailwindcss";
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [tailwind("tailwind.config.cjs")],
+    },
+  }),
+};

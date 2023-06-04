f9f4240be279db7cd8e97fda15852077eb70b719
Guillaume <g.fortin@sollum.tech>
2023-06-03 21:04:06 -0400

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit()]/**,
 	build: {
 	    rollupOptions: {
 	        entryFileNames: `assets/[name].js`,
 	        chunkFileNames: `assets/[name].js`
 	    }
-  	}
+  	}**/
 };
 
 export default config;

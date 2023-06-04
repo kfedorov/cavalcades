0ac81e5a404f94022385b78e3d1aad4257180829
Guillaume <g.fortin@sollum.tech>
2023-06-03 20:53:43 -0400

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,15 @@ const config = {
 };
 
 export default config;
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
